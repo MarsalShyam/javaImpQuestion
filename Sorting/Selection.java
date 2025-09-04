d9dadc203cd0a7909011097ebed16907c1a007ff
MarsalShyam <shyam12marsal@gmail.com>
2025-09-05 05:22:41 +0530

    adding insertion sort

--- Sorting/Selection.java
+++ Sorting/Selection.java
@@ -1,5 +1,24 @@
+//Insertion Sort
 class Selection{
+    public static void printArr(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
     public static void main(String[] args) {
-    System.err.println("    ");
+    // System.err.println("    ");
+    int arr[]={32,3,2,6,7,3,39};
+    for(int i=0;i<arr.length;i++){
+        int smallest=i;
+        for(int j=i+1;j<arr.length;j++){
+            if(arr[smallest]>arr[j]){
+                smallest=j;
+            }
+        }
+        int temp=arr[smallest];
+        arr[smallest]=arr[i];
+        arr[i]=temp;
+    }
+    printArr(arr);
     }
 }
